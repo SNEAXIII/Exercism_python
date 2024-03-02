561c2fc931c9f19452ae23f09ccab91264845eef
AlexisB <misterbalise2@gmail.com>
2024-03-02 16:23:58 +0100

    remove useless reference to em & strong beacon

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -39,46 +39,17 @@ def parse(markdown: str):
             if not in_list:
                 in_list = True
                 curr = m.group(1)
-                m1 = re.match('^(.*)__(.*)__(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<strong>' + \
-                           m1.group(2) + '</strong>' + m1.group(3)
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                           '</em>' + m1.group(3)
                 line = '<ul><li>' + curr + '</li>'
             else:
-                is_bold = False
-                is_italic = False
                 curr = m.group(1)
-                m1 = re.match('(.*)__(.*)__(.*)', curr)
-                if m1:
-                    is_bold = True
-                m1 = re.match('(.*)_(.*)_(.*)', curr)
-                if m1:
-                    is_italic = True
-                if is_bold:
-                    curr = m1.group(1) + '<strong>' + \
-                           m1.group(2) + '</strong>' + m1.group(3)
-                if is_italic:
-                    curr = m1.group(1) + '<em>' + m1.group(2) + \
-                           '</em>' + m1.group(3)
                 line = '<li>' + curr + '</li>'
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
-
         m = re.match('<h|<ul|<p|<li', line)
         if not m:
             line = '<p>' + line + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', line)
-        if m:
-            line = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', line)
-        if m:
-            line = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
         if in_list_append:
             line = '</ul>' + line
             in_list_append = False
