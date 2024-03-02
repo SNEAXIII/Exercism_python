3dc99594238632644725ffc651cb282c40ac2f6b
AlexisB <misterbalise2@gmail.com>
2024-03-02 16:20:10 +0100

    add tranform_strong_em

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,23 +1,39 @@
 import re
 
-print(re.search("#* ", "### test").group())
-
 
 def transform_header(line: str):
     find = re.search("^#{1,6}\s", line)
-    if find:
-        count = len(find.group())
-        return f"<h{count - 1}>{line[count:]}</h{count - 1}>"
+    if not find:
+        return line
+    count = len(find.group())
+    return f"<h{count - 1}>{line[count:]}</h{count - 1}>"
+
+
+dict_to_replace = {
+    "__": ("<strong>", "</strong>"),
+    "_": ("<em>", "</em>")
+}
+
+
+def tranform_strong_em(line: str):
+    for symbol, tuple_substitute in dict_to_replace.items():
+        count = line.count(symbol)
+        line = line.replace(symbol, "%s", count // 2 * 2)
+        line %= tuple_substitute * (count // 2)
     return line
 
 
-def parse(markdown):
+def parse(markdown: str):
+    print()
+    number_of_star = markdown.count("*")
+    print(f"{number_of_star = }")
     lines = markdown.split('\n')
     res = ''
     in_list = False
     in_list_append = False
     for line in lines:
         line = transform_header(line)
+        line = tranform_strong_em(line)
         m = re.match(r'\* (.*)', line)
         if m:
             if not in_list:
