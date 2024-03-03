96d1f33a764c7e0788a8208acde31e30469ef6d1
AlexisB <misterbalise2@gmail.com>
2024-03-03 23:32:19 +0100

    mergeing

--- python/ledger/ledger.py
+++ python/ledger/ledger.py
@@ -1,6 +1,11 @@
 from datetime import datetime
 from operator import attrgetter
 
+EUR = "EUR"
+USD = "USD"
+NLNL = "nl_NL"
+ENUS = "en_US"
+
 
 class LedgerEntry:
     def __init__(self):
@@ -18,12 +23,18 @@ class LedgerEntry:
     def get_format_time(self, time_format: str) -> str:
         return self.date.strftime(time_format)
 
-    def get_format_change_us(self,locale, currency: str) -> str:
-        money = f"{abs(self.change / 100):,.2f}"
-        if self.change >= 0:
-            str_change = f"{currency}{money} "
-        else:
-            str_change = f"({currency}{money})"
+    def get_format_change(self, locale, currency: str) -> str:
+        if locale == ENUS:
+            money = f"{abs(self.change) / 100:,.2f}"
+            if self.change >= 0:
+                str_change = f"{currency}{money} "
+            else:
+                str_change = f"({currency}{money})"
+        elif locale == NLNL:
+            money = f"{self.change / 100:,.2f}"
+            money = money.replace(",", "_").replace(".", ",").replace("_", ".")
+            str_change = f"{currency} {money} "
+
         return f"{str_change:>13}"
 
 
@@ -40,12 +51,12 @@ def create_entry(date, description, change):
 
 
 translate_currency = {
-    "USD": "$",
-    "EUR": "€"
+    USD: "$",
+    EUR: "€"
 }
 translate_languages = {
-    "en_US": ("Date", "Description", "Change", "%m/%d/%Y"),
-    "nl_NL": ("Datum", "Omschrijving", "Verandering", "%Y/%m/%d")
+    ENUS: ("Date", "Description", "Change", "%m/%d/%Y"),
+    NLNL: ("Datum", "Omschrijving", "Verandering", "%d-%m-%Y")
 }
 
 
@@ -54,145 +65,26 @@ def format_entries(currency, locale, entries):
     entries = multi_sort(entries)
     date, description, change, time_format = translate_languages[locale]
     table = f"{date:<10} | {description:<25} | {change:<13}"
-    if locale == "en_US":
-        currency = translate_currency[currency]
 
-        for entry in entries:
-            table += "\n"
+    currency = translate_currency[currency]
 
-            date_str = entry.get_format_time(time_format)
-            table += date_str
-            table += " | "
+    for entry in entries:
+        table += "\n"
 
-            description = f"{entry:str_concat_description}"
-            table += description
-            table += " | "
+        date_str = entry.get_format_time(time_format)
+        print(date_str)
+        table += date_str
+        table += " | "
 
-            str_change = entry.get_format_change_us(locale,currency)
-            table += str_change
-        print("\n" + table)
-        return table
-    elif locale == "nl_NL":
+        description = f"{entry:str_concat_description}"
+        table += description
+        table += " | "
 
-        while len(entries) > 0:
-            table += "\n"
+        str_change = entry.get_format_change(locale, currency)
+        table += str_change
+    print("\n" + table)
+    return table
 
-            min_entry_index = -1
-            for i in range(len(entries)):
-                entry = entries[i]
-                if min_entry_index < 0:
-                    min_entry_index = i
-                    continue
-                min_entry = entries[min_entry_index]
-                if entry.date < min_entry.date:
-                    min_entry_index = i
-                    continue
-                if (
-                        entry.date == min_entry.date and
-                        entry.change < min_entry.change
-                ):
-                    min_entry_index = i
-                    continue
-                if (
-                        entry.date == min_entry.date and
-                        entry.change == min_entry.change and
-                        entry.description < min_entry.description
-                ):
-                    min_entry_index = i
-                    continue
-            entry = entries[min_entry_index]
-            entries.pop(min_entry_index)
-
-            day = entry.date.day
-            day = str(day)
-            if len(day) < 2:
-                day = "0" + day
-            date_str = day
-            date_str += "-"
-            month = entry.date.month
-            month = str(month)
-            if len(month) < 2:
-                month = "0" + month
-            date_str += month
-            date_str += "-"
-            year = entry.date.year
-            year = str(year)
-            while len(year) < 4:
-                year = "0" + year
-            date_str += year
-            table += date_str
-            table += " | "
-
-            if len(entry.description) > 25:
-                for i in range(22):
-                    table += entry.description[i]
-                table += "..."
-            else:
-                for i in range(25):
-                    if len(entry.description) > i:
-                        table += entry.description[i]
-                    else:
-                        table += " "
-            table += " | "
-
-            if currency == "USD":
-                change_str = "$ "
-                if entry.change < 0:
-                    change_str += "-"
-                change_dollar = abs(int(entry.change / 100.0))
-                dollar_parts = []
-                while change_dollar > 0:
-                    dollar_parts.insert(0, str(change_dollar % 1000))
-                    change_dollar = change_dollar // 1000
-                if len(dollar_parts) == 0:
-                    change_str += "0"
-                else:
-                    while True:
-                        change_str += dollar_parts[0]
-                        dollar_parts.pop(0)
-                        if len(dollar_parts) == 0:
-                            break
-                        change_str += "."
-                change_str += ","
-                change_cents = abs(entry.change) % 100
-                change_cents = str(change_cents)
-                if len(change_cents) < 2:
-                    change_cents = "0" + change_cents
-                change_str += change_cents
-                change_str += " "
-                while len(change_str) < 13:
-                    change_str = " " + change_str
-                table += change_str
-            elif currency == "EUR":
-                change_str = u"€ "
-                if entry.change < 0:
-                    change_str += "-"
-                change_euro = abs(int(entry.change / 100.0))
-                euro_parts = []
-                while change_euro > 0:
-                    euro_parts.insert(0, str(change_euro % 1000))
-                    change_euro = change_euro // 1000
-                if len(euro_parts) == 0:
-                    change_str += "0"
-                else:
-                    while True:
-                        change_str += euro_parts[0]
-                        euro_parts.pop(0)
-                        if len(euro_parts) == 0:
-                            break
-                        change_str += "."
-                change_str += ","
-                change_cents = abs(entry.change) % 100
-                change_cents = str(change_cents)
-                if len(change_cents) < 2:
-                    change_cents = "0" + change_cents
-                change_str += change_cents
-                change_str += " "
-                while len(change_str) < 13:
-                    change_str = " " + change_str
-                table += change_str
-        print("\n" + table)
-        return table
 
 
 currency = "USD"
