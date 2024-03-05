18b99594e96a2e54d8502872333795344b0dfa19
AlexisB <misterbalise2@gmail.com>
2024-03-06 00:41:04 +0100

    1ere version non fonctionnelle grep.py

--- python/grep/grep.py
+++ python/grep/grep.py
@@ -1,2 +1,29 @@
+def check_and_transform_if_match_entiere_line(line: str, pattern: str, is_case_sensitive: bool) -> str:
+    copy_line = line
+    if not is_case_sensitive:
+        line, pattern = line.lower(), pattern.lower()
+    if pattern + "\n" == line:
+        return copy_line
+    return ""
+
+
+def check_and_transform_if_str_contain_substr(line: str, pattern: str, is_case_sensitive: bool) -> str:
+    copy_line = line
+    if not is_case_sensitive:
+        line, pattern = line.lower(),pattern.lower()
+    if pattern in line:
+        return copy_line
+    return ""
+
+
 def grep(pattern, flags, files):
-    pass
+    is_reverse_condition = "-v" in flags
+    is_case_sensitive = "-i" not in flags
+    check_and_transform_if_match_method = check_and_transform_if_match_entiere_line if "-x" in flags else check_and_transform_if_str_contain_substr
+
+    for file in files:
+        with open(file, encoding="utf-8") as content:
+            to_return = ""
+            for index,line in enumerate(content.readlines()):
+                to_return += check_and_transform_if_match_method(line,pattern,is_case_sensitive)
+    return to_return
