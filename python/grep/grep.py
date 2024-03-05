1c04a79f0fe7290683861e839be50869100e16aa
AlexisB <misterbalise2@gmail.com>
2024-03-06 00:52:43 +0100

    need to implement -v and -l

--- python/grep/grep.py
+++ python/grep/grep.py
@@ -1,29 +1,25 @@
-def check_and_transform_if_match_entiere_line(line: str, pattern: str, is_case_sensitive: bool) -> str:
-    copy_line = line
-    if not is_case_sensitive:
-        line, pattern = line.lower(), pattern.lower()
-    if pattern + "\n" == line:
-        return copy_line
-    return ""
-
-
-def check_and_transform_if_str_contain_substr(line: str, pattern: str, is_case_sensitive: bool) -> str:
-    copy_line = line
-    if not is_case_sensitive:
-        line, pattern = line.lower(),pattern.lower()
-    if pattern in line:
-        return copy_line
-    return ""
 
 
 def grep(pattern, flags, files):
     is_reverse_condition = "-v" in flags
     is_case_sensitive = "-i" not in flags
-    check_and_transform_if_match_method = check_and_transform_if_match_entiere_line if "-x" in flags else check_and_transform_if_str_contain_substr
-
+    is_full_match = "-x" in flags
+    is_add_number = "-n" in flags
+    to_return = ""
     for file in files:
         with open(file, encoding="utf-8") as content:
-            to_return = ""
-            for index,line in enumerate(content.readlines()):
-                to_return += check_and_transform_if_match_method(line,pattern,is_case_sensitive)
+
+            for index, line in enumerate(content.readlines()):
+                copy_line = line
+                if not is_case_sensitive:
+                    line, pattern = line.lower(), pattern.lower()
+                if is_full_match:
+                    if pattern + "\n" != line:
+                        copy_line = ""
+                else:
+                    if pattern not in line:
+                        copy_line = ""
+                if is_add_number and copy_line:
+                    copy_line = f"{index+1}:{copy_line}"
+                to_return += copy_line
     return to_return
