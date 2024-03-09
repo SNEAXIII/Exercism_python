30ba948fc7deaab8e89528a4064a63ebad319ea2
AlexisB <misterbalise2@gmail.com>
2024-03-09 17:50:12 +0100

    add -v param + fix prefix

--- python/grep/grep.py
+++ python/grep/grep.py
@@ -1,25 +1,36 @@
+def checkAndAddLine(is_full_match, pattern, line):
+    # print("is_full_match,pattern,line --> ", is_full_match, pattern, line)
+    if is_full_match and pattern + "\n" == line:
+        return True
+    elif not is_full_match and pattern in line:
+        return True
+    return False
 
 
 def grep(pattern, flags, files):
-    is_reverse_condition = "-v" in flags
-    is_case_sensitive = "-i" not in flags
-    is_full_match = "-x" in flags
-    is_add_number = "-n" in flags
+    is_multiple_files: bool = 1 < len(files)
+    is_reverse_condition: bool = "-v" in flags
+    is_case_sensitive: bool = "-i" not in flags
+    is_full_match: bool = "-x" in flags
+    is_add_number: bool = "-n" in flags
     to_return = ""
-    for file in files:
-        with open(file, encoding="utf-8") as content:
-
-            for index, line in enumerate(content.readlines()):
-                copy_line = line
-                if not is_case_sensitive:
-                    line, pattern = line.lower(), pattern.lower()
-                if is_full_match:
-                    if pattern + "\n" != line:
-                        copy_line = ""
-                else:
-                    if pattern not in line:
-                        copy_line = ""
-                if is_add_number and copy_line:
-                    copy_line = f"{index+1}:{copy_line}"
-                to_return += copy_line
+    for file_name in files:
+        with open(file_name, encoding="utf-8") as file:
+            file_content = file.readlines()
+        for index, line in enumerate(file_content):
+            prefix = ""
+            copy_line = line
+            if not is_case_sensitive:
+                line, pattern = line.lower(), pattern.lower()
+            is_grep: bool = checkAndAddLine(is_full_match, pattern, line)
+            # print(f"{is_grep = }")
+            if is_reverse_condition:
+                is_grep = not is_grep
+            if not is_grep:
+                continue
+            if is_multiple_files:
+                prefix += f"{file_name}:"
+            if is_add_number:
+                prefix += f"{index + 1}:"
+            to_return += prefix + copy_line
     return to_return
