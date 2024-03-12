4c1b36c9e92f6f70428bc2d5d92b5110c49c0bf5
AlexisB <misterbalise2@gmail.com>
2024-03-13 00:30:20 +0100

    finish

--- python/word-search/word_search.py
+++ python/word-search/word_search.py
@@ -1,10 +1,18 @@
 from itertools import product
 
+DIRECTIONS = {
+    "south": (0, 1),
+    "north": (0, -1),
+    "east": (1, 0),
+    "weast": (-1, 0),
+    "south-east": (1, 1),
+    "south-weast": (1, -1),
+    "north-east": (-1, 1),
+    "north-weast": (-1, -1),
+}
+
 
 class Point:
-    def __repr__(self):
-        return f"{self.x = }, {self.y = }"
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -14,15 +22,17 @@ class Point:
 
 
 class WordSearch:
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.y_size = len(puzzle)
+        self.x_size = len(puzzle[0])
 
     def universal_check(self, word, point, x_coef, y_coef):
         size = len(word)
         x, y = point.x, point.y
         x_new = x + (x_coef * size) - x_coef
-        if any((x_new >= self.x_size, x_new < 0)):
-            return False
         y_new = y + (y_coef * size) - y_coef
-        if any((y_new >= self.x_size, y_new < 0)):
+        if not (0 <= x_new < self.x_size and 0 <= y_new < self.y_size):
             return False
         extract = self.universal_extract(size, point, x_coef, y_coef)
         if extract == word:
@@ -38,12 +48,7 @@ class WordSearch:
             extracted_word += selected_letter
         return extracted_word
 
-    def __init__(self, puzzle):
-        self.puzzle = puzzle
-        self.y_size = len(puzzle)
-        self.x_size = len(puzzle[0])
-
-    def find_all_one_letter_in_a_dict(self, letter):
+    def find_one_letter_indices_in_list_points(self, letter):
         indices = []
         for y, line in enumerate(self.puzzle):
             x = line.find(letter)
@@ -54,9 +59,7 @@ class WordSearch:
 
     def search(self, word):
         first_letter = word[0]
-        coefs = (-1, 0, 1)
-        combinaison = [(x, y) for x, y in product(coefs, coefs) if any((x, y))]
-        indices = self.find_all_one_letter_in_a_dict(first_letter)
-        for (x_coef, y_coef), start_point in product(combinaison, indices):
+        indices = self.find_one_letter_indices_in_list_points(first_letter)
+        for (x_coef, y_coef), start_point in product(DIRECTIONS.values(), indices):
             if result := self.universal_check(word, start_point, x_coef, y_coef):
                 return result
