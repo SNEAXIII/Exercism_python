692e16569e8198481cd5cf2a38c4d9076640005d
AlexisB <misterbalise2@gmail.com>
2024-03-30 00:25:35 +0100

    Make a better word search

--- python/word-search/word_search.py
+++ python/word-search/word_search.py
@@ -1,54 +1,49 @@
+from dataclasses import dataclass
 from itertools import product
 
-DIRECTIONS = {
-    "south": (0, 1),
-    "north": (0, -1),
-    "east": (1, 0),
-    "weast": (-1, 0),
-    "south-east": (1, 1),
-    "south-weast": (1, -1),
-    "north-east": (-1, 1),
-    "north-weast": (-1, -1),
-}
+values = (-1, 1, 0)
+DIRECTIONS = list(product(values, values))
+# We don't need (0,0) direction
+DIRECTIONS.pop()
 
 
+@dataclass
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    x: int
+    y: int
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
 
 class WordSearch:
-    def __init__(self, puzzle):
+    def __init__(self, puzzle: list):
         self.puzzle = puzzle
         self.y_size = len(puzzle)
         self.x_size = len(puzzle[0])
 
-    def universal_check(self, word, point, x_coef, y_coef):
-        size = len(word)
-        x, y = point.x, point.y
-        x_new = x + (x_coef * size) - x_coef
-        y_new = y + (y_coef * size) - y_coef
-        if not (0 <= x_new < self.x_size and 0 <= y_new < self.y_size):
-            return False
-        extract = self.universal_extract(size, point, x_coef, y_coef)
-        if extract == word:
-            return Point(x, y), Point(x_new, y_new)
-        return False
-
-    def universal_extract(self, size, point_base, x_coef, y_coef):
-        extracted_word = ""
-        for index in range(size):
+    def single_direction_find(self, word: str, point_base: Point, x_coef: int, y_coef: int) -> bool | None:
+        for index, expected_letter in enumerate(word):
             x_actual = point_base.x + (index * x_coef)
             y_actual = point_base.y + (index * y_coef)
-            selected_letter = self.puzzle[y_actual][x_actual]
-            extracted_word += selected_letter
-        return extracted_word
+            actual_letter = self.puzzle[y_actual][x_actual]
+            if actual_letter != expected_letter:
+                return None
+        return True
 
-    def find_one_letter_indices_in_list_points(self, letter):
+    def single_direction_check(self, word: str, point: Point, x_coef: int, y_coef: int) \
+            -> bool | tuple[Point, Point] | None:
+        size = len(word)
+        x, y = point.x, point.y
+        x_new = x - x_coef + x_coef * size
+        y_new = y - y_coef + y_coef * size
+        if not (0 <= x_new < self.x_size and 0 <= y_new < self.y_size):
+            return False
+        if self.single_direction_find(word, point, x_coef, y_coef):
+            return Point(x, y), Point(x_new, y_new)
+        return None
+
+    def find_one_letter_indices_in_list_points(self, letter: str) -> list:
         indices = []
         for y, line in enumerate(self.puzzle):
             x = line.find(letter)
@@ -57,9 +52,9 @@ class WordSearch:
                 x = line.find(letter, x + 1)
         return indices
 
-    def search(self, word):
+    def search(self, word: str) -> bool | None:
         first_letter = word[0]
         indices = self.find_one_letter_indices_in_list_points(first_letter)
-        for (x_coef, y_coef), start_point in product(DIRECTIONS.values(), indices):
-            if result := self.universal_check(word, start_point, x_coef, y_coef):
+        for (x_coef, y_coef), start_point in product(DIRECTIONS, indices):
+            if result := self.single_direction_check(word, start_point, x_coef, y_coef):
                 return result
